96deefea7a508da7de9e9bff9e4d0d1e4b7535a4
Arpit Taksande <145291077+AAtaksan@users.noreply.github.com>
2026-07-11 15:27:54 +0530

    Add background worker startup to entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -28,5 +28,8 @@ PY
 echo "Running migrations..."
 alembic upgrade head
 
+echo "Starting background worker..."
+arq app.workers.worker.WorkerSettings &
+
 echo "Starting API..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
